15286571a169d0d5c6857a75c12edc9e0184b15a
prathapallu <prathap.a5233@gmail.com>
2025-07-05 16:24:00 -0700

    -Image processing and basic exporting feature

--- paperfy/ViewModels/ImageEditorViewModel.cs
+++ paperfy/ViewModels/ImageEditorViewModel.cs
@@ -2,12 +2,23 @@
 using ReactiveUI;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 
 namespace Paperfy.ViewModels
 {
     public class ImageEditorViewModel : ViewModelBase
     {
         private ObservableCollection<byte[]> _images;
+
+        private byte[] _selectedImage;
+
+        public byte[] SelectedImage
+        {
+            get => _selectedImage;
+            set => this.RaiseAndSetIfChanged(ref _selectedImage, value);
+        }
+
+        public ICommand SelectImageCommand { get; }
 
         public ObservableCollection<byte[]> Images
         {
@@ -18,6 +29,11 @@ namespace Paperfy.ViewModels
         public ImageEditorViewModel()
         {
             LoadImages();
+            // Set first image as selected by default
+            if (Images?.Any() == true)
+                SelectedImage = Images.First();
+
+            SelectImageCommand = ReactiveCommand.Create<byte[]>(image => SelectedImage = image);
         }
 
         private void LoadImages()
